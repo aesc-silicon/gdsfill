4b64dc34c3fae776d65cc11b9d9053a2c12cc940
Daniel Schultz <dnltz@aesc-silicon.de>
2025-09-19 13:59:11 +0200

    gdsfill: Update Console Output
    Reformat the console output and update previous printed lines.
    For example, update the tile specific line as soon as a tile
    got successfully prepared or filled.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- gdsfill/library/fill.py
+++ gdsfill/library/fill.py
@@ -19,7 +19,8 @@ ALGOS = {
 }
 
 
-def fill_layer(pdk, inputfile, layer, tiles, tile):
+# pylint: disable=too-many-arguments, too-many-positional-arguments, too-many-locals
+def fill_layer(pdk, inputfile, layer, queue, tiles, tile):
     """
     Fill a layout layer to meet density requirements.
 
@@ -30,6 +31,7 @@ def fill_layer(pdk, inputfile, layer, tiles, tile):
         pdk (object): Provides layer rules and supported algorithms.
         inputfile (Path | str): Path to the input GDS file.
         layer (str): Target layer name.
+        queue (Queue): Queue object to return results.
         tiles (dict): Tiling information for the layout.
         tile (object): Current tile instance.
 
@@ -43,26 +45,28 @@ def fill_layer(pdk, inputfile, layer, tiles, tile):
     desired_density = pdk.get_layer_density(layer)
     fill_algos = pdk.get_layer_algorithm(layer)
 
-    print(f"Filling Tile {tile.x}x{tile.y}")
-    print(f"Metal density: {metal_density} %")
-    print(f"Desired density: {desired_density} % with {pdk.get_layer_deviation(layer)} % deviation")
-    print(f"Fill algorithm: {', '.join(fill_algos)}")
-
     for fill_algo in fill_algos:
         if fill_algo not in ALGOS:
-            print(f"Unknown fill algorithm {fill_algo} for layer {layer}")
+            queue.put(("error", f"Unknown fill algorithm {fill_algo} for layer {layer}"))
             return False
 
     for fill_algo in fill_algos:
         if not pdk.has_fill_algorithm(layer, fill_algo):
-            print(f"Unsupported fill algorithm {fill_algo} for layer {layer}")
+            queue.put(("error", f"Unsupported fill algorithm {fill_algo} for layer {layer}"))
             return False
 
+    msg = f"Metal density {metal_density: <5} % (target {desired_density} %)"
     fill_lib = gdstk.Library("fill")
     if metal_density < desired_density:
+        fills = []
         for fill_algo in fill_algos:
-            fill_cell = ALGOS[fill_algo](pdk, layer, tiles, tile, annotated_cell)
+            fill_cell, result = ALGOS[fill_algo](pdk, layer, tiles, tile, annotated_cell)
             fill_lib.add(fill_cell)
+            fills.append((fill_algo, result))
+        final_fill = round(metal_density + sum(fill for _, fill in fills), 2)
+        queue.put(("success", f"{msg} - final density {final_fill} % "))
+    else:
+        queue.put(("skipped", f"{msg} - exceeds limit"))
 
     fill_lib.write_gds(str(inputfile).replace('modified', 'filled'))
     return True
